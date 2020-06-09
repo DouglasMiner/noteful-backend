92142e6671e8f909a348facfc7bfa02906417c14
Douglas Miner <curr3nas@gmail.com>
2020-06-09 05:51:29 -0500

    ready to deploy

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,8 +3,9 @@ require("dotenv").config();
 module.exports = {
   migrationsDirectory: "migrations",
   driver: "pg",
+  ssl: { rejectUnauthorized: false },
   connectionString:
     process.env.NODE_ENV === "test"
-      ? process.env.TEST_DB_URL
-      : process.env.DB_URL,
+      ? process.env.TEST_DATABASE_URL
+      : process.env.DATABASE_URL,
 };
