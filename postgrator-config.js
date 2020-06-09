c13443e7ba6389746449fe09d9ca48d4e469c033
Douglas Miner <curr3nas@gmail.com>
2020-06-09 06:13:47 -0500

    rolled back pg to 7.8.2

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
 module.exports = {
-  migrationsDirectory: "migrations",
-  driver: "pg",
-  ssl: { rejectUnauthorized: false },
+  "migrationsDirectory": "migrations",
+  "driver": "pg",
   connectionString:
     process.env.NODE_ENV === "test"
       ? process.env.TEST_DATABASE_URL
       : process.env.DATABASE_URL,
+  "ssl": !!process.env.SSL,
 };
